6b11f611053bc217811db89dc09d013d856ee176
Max Melching <m-melching@web.de>
2025-02-13 20:41:24 +0100

    Adding versioning and correcting setup name. Also adding gitignore.

--- setup.py
+++ setup.py
@@ -6,19 +6,32 @@ setup.py file for waveform-errors from pycbc waveform plugin package
 from setuptools import Extension, setup, Command
 from setuptools import find_packages
 
+version_config = {
+    'version_file': '_pycbc_wferrors_version.py',
+    'version_scheme': 'no-guess-dev',
+    'local_scheme': 'dirty-tag',
+    'fallback_version': 'unknown'
+}
 
-setup (
-    name = 'pycbc-wferrors_test',
-    description = 'An example waveform plugin for PyCBC',
+
+setup(
+    name = 'pycbc-wferrors',
+    use_scm_version = version_config,
+    description = 'An waveform plugin for systematic errors for PyCBC',
     long_description = open('descr.rst').read(),
     author = 'The PyCBC team',
     author_email = 'sumit.kumar@aei.mpg.de',
     url = 'http://www.pycbc.org/',
     keywords = ['pycbc', 'signal processing', 'gravitational waves'],
+    setup_requires = [
+        'setuptools>=64',
+        'setuptools_scm>=8',
+        'wheel',
+    ],
     install_requires = ['pycbc'],
     py_modules = ['wferrors'],
     entry_points = {"pycbc.waveform.fd":"wferrors = wferrors:amplitude_phase_modification_fd"},
-    classifiers=[
+    classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
